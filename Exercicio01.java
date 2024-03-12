8564095d54351d9e70cb37a8cb33ab701bf93cc0
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-11 21:16:55 -0300

    Primeiro programa

---
+++ Exercicio01.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio01 {
+
+    public static void executar(){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Informe número");
+        String numero = leitor.nextLine();
+
+        
+
+        leitor.close();
+    }   
+
+}
