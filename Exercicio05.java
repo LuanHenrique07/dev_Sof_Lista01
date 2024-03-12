df94aadff67a8ce9e3b135baf87634eafed007fb
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-12 00:08:19 -0300

    Quinto exercicio

---
+++ Exercicio05.java
@@ -0,0 +1,14 @@
+public class Exercicio05 {
+    public static void executar(){
+
+        double A = Prompt.lerDecimal("Digite o primeiro número: ");
+        double B = Prompt.lerDecimal("Digite o segundo número: ");
+
+        double C = A;
+        A = B; 
+        B = C;
+
+        Prompt.imprimir("A ordem das variaveis correta:  " + C + " e " + A);
+        Prompt.imprimir("A ordem das variaveis Trocadas:  " + A + " e " + B);
+    } 
+}
