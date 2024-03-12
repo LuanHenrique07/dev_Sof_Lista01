9fb7787ebd0181943aecf750848c26f0e90f63cd
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-11 23:52:45 -0300

    Quarto Exercicio

---
+++ Exercicio04.java
@@ -0,0 +1,18 @@
+public class Exercicio04 {
+    public static void executar(){
+
+        double A = Prompt.lerDecimal("Digite o primeiro número: ");
+        double B = Prompt.lerDecimal("Digite o segundo número: ");
+
+        double soma = A + B;
+        double sub =  A - B;
+        double mult = A * B;
+        double div =  A / B;
+
+        Prompt.imprimir("A soma dos numeros e: " + soma);
+        Prompt.imprimir("A subtracao dos numeros e: " + sub);
+        Prompt.imprimir("A multiplicacao dos numeros e: " + mult);
+        Prompt.imprimir("A divisao dos numeros e: " + div);
+        
+    } 
+}
