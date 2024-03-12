d6e0485afb2335ec5ead5fcbb57be08f588dd583
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-11 21:28:39 -0300

    Exercicio 1

--- Exercicio01.java
+++ Exercicio01.java
@@ -1,16 +1,16 @@
-import java.util.Scanner;
+
 
 public class Exercicio01 {
 
     public static void executar(){
-        Scanner leitor = new Scanner(System.in);
-
-        System.out.println("Informe número");
-        String numero = leitor.nextLine();
+        double numero= Prompt.lerDecimal("Digite o número: ");
+        if (numero > 10) {
+            Prompt.imprimir("Número maior do que 10!");
+        } else{
+            Prompt.imprimir("Número menor ou igual a 10!");
+        }
 
         
-
-        leitor.close();
     }   
 
 }
