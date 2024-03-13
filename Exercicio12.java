0a84bec3c938ed9b44a0a026b43c82497d26f807
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-13 16:44:44 -0300

    Decimo Segundo Exercicio

---
+++ Exercicio12.java
@@ -0,0 +1,48 @@
+public class Exercicio12 {
+
+    public static void executar(){
+        int mes = Prompt.lerInteiro("Informe o mes em numero: ");
+
+        switch (mes) {
+            case 1:
+                Prompt.imprimir("Janeiro");
+                break;
+            case 2:
+                Prompt.imprimir("Fevereiro");
+                break;
+            case 3:
+                Prompt.imprimir("Marco");
+                break;
+            case 4:
+                Prompt.imprimir("Abril");
+                break;
+            case 5:
+                Prompt.imprimir("Maio");
+                break;
+            case 6:
+                Prompt.imprimir("Junho");
+                break;
+            case 7:
+                Prompt.imprimir("Julho");
+                break;
+            case 8:
+                Prompt.imprimir("Agosto");
+                break;
+            case 9:
+                Prompt.imprimir("Setembro");
+                break;
+            case 10:
+                Prompt.imprimir("Outubro");
+                break;
+            case 11:
+                Prompt.imprimir("Novembro");
+                break;
+            case 12:
+                Prompt.imprimir("Dezembro");
+                break;
+            default:
+                Prompt.imprimir("Mes invalido!");
+                break;
+        }
+    }
+}
