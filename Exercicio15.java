c170b17c1204b91f25efd9e646f9cde7ab20fbbd
Luan Almeida <luan.henrique.almeida@hotmail.com.br>
2024-03-14 15:20:18 -0300

    Decimo sexto exercicio

---
+++ Exercicio15.java
@@ -0,0 +1,16 @@
+public class Exercicio15 {
+
+    public static void executar(){
+        double x1 = Prompt.lerDecimal("Informe o ponto x1: ");
+        double y1 = Prompt.lerInteiro("Informe o ponto y1: ");
+        double x2 = Prompt.lerInteiro("Informe o ponto x2: ");
+        double y2 = Prompt.lerInteiro("Informe o ponto y1: ");
+
+        double sub1 = x2 - x1;
+        double sub2 = y2 - y1;
+
+        double dist = Math.sqrt((Math.pow(sub1,2)) + (Math.pow(sub2, 2)));
+
+        Prompt.imprimir("A distancia desses pontos e: " + dist);
+    }
+}
